85c44a308ca71cbd52aeb59c88df31653f33cabf
Tin Ha <hatin1995@gmail.com>
2022-02-21 01:17:13 -0800

    dark mode switch added

--- next-amazon/pages/product/[slug].js
+++ next-amazon/pages/product/[slug].js
@@ -43,7 +43,9 @@ export default function Product() {
         <Grid item md={3} xs={12}>
           <List>
             <ListItem>
-              <Typography component="h1">{product.name}</Typography>
+              <Typography component="h1" variant="h1">
+                {product.name}
+              </Typography>
             </ListItem>
             <ListItem>
               <Typography>Category: {product.category}</Typography>
